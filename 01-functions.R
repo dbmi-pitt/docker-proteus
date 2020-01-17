ce8d0676f4d227215d3d569faa16006e9ff1a28a
Patrick O'Halloran <pohalloran1@gmail.com>
2020-01-17 12:54:27 -0500

    column renaming bug fix in summarization fn

--- 01-functions.R
+++ 01-functions.R
@@ -1381,10 +1381,10 @@ generate_summary <- function(conn, backend = NULL, version = NULL, schema = NULL
   #' field: name of column to filter over, required if filtered = TRUE
   #' value: name of value to filter in given field, required if filtered = TRUE
   if (version == "4.1" | version == "4.1_STG") {
-    metadata <- readr::read_csv('/app/inst/CDM_41_field_names.csv')
+    metadata <- readr::read_csv('./inst/CDM_41_field_names.csv')
   }
   if (version == "5.1" | version == "5.1_STG") {
-    metadata <- readr::read_csv('/app/inst/CDM_51_metadata.csv')
+    metadata <- readr::read_csv('./inst/CDM_51_metadata.csv')
   }
   # get column info to decide what queries to run
   rs <- DBI::dbSendQuery(conn, paste0("SELECT * FROM ", ifelse(backend=="Oracle", paste0(schema, ".", table), table)))
@@ -1411,6 +1411,7 @@ generate_summary <- function(conn, backend = NULL, version = NULL, schema = NULL
           summarize_if(is.numeric, list(cn = ~ count(), nNULL = ~ sum(case_when(is.na(.) ~ 1, !is.na(.) ~ 0)),
                                         nd = n_distinct, min = min, mean = mean, max = max)) %>%
           collect() %>%
+          {if (sum(1*(colinfo$type==6))==1) rename_all(., .funs = list(~paste0(gsub("\\_", "", colinfo[colinfo$type==6,]$name), "_", .))) else . } %>%
           bind_cols(
             {if(backend == "Oracle") tbl(conn, dbplyr::in_schema(schema, table)) else tbl(conn, table)} %>% select_if(is.numeric) %>% head(5) %>% collect() %>% names %>%
               map(~ summarize_distribution(conn, backend, schema, table, field = .x)) %>%
@@ -1423,7 +1424,7 @@ generate_summary <- function(conn, backend = NULL, version = NULL, schema = NULL
     mutate(pct_null = round(as.numeric(nNULL) / as.numeric(cn), 3) * 100,
            pct_dist = round(as.numeric(nd) / as.numeric(cn), 3) * 100,
            pct_missing = round(as.numeric(nNI) / as.numeric(cn), 3) * 100) %>%
-    left_join(., readr::read_csv('/app/inst/CDM_51_metadata.csv') %>%
+    left_join(., readr::read_csv('./inst/CDM_51_metadata.csv') %>%
                 filter(Table == table) %>%
                 select(key, Field:Required),
               by = 'key') %>%
@@ -1431,9 +1432,9 @@ generate_summary <- function(conn, backend = NULL, version = NULL, schema = NULL
     select(-key, -Order) %>%
     select(Field, Required, everything()) %T>%
     purrr::when(filtered == TRUE ~ write.csv(.,
-                                             file = paste0('/app/summaries/CSV/', table, "_", field, "_", value, '.csv'),
+                                             file = paste0('./summaries/CSV/', table, "_", field, "_", value, '.csv'),
                                              row.names = FALSE),
-                ~ write.csv(., file = paste0('/app/summaries/CSV/', table, '.csv'), row.names = FALSE)
+                ~ write.csv(., file = paste0('./summaries/CSV/', table, '.csv'), row.names = FALSE)
     ) %>%
     purrr::when(sum(1*(colinfo$type==6))>0
                 ~ select(., Field, Required, cn, nd, pct_dist, nNULL, pct_null, nNI, pct_missing,
@@ -1453,9 +1454,9 @@ generate_summary <- function(conn, backend = NULL, version = NULL, schema = NULL
                   )
     ) %>%
     purrr::when(filtered == TRUE ~ htmlwidgets::saveWidget(.,
-                                                           paste0(normalizePath('/app/summaries/HTML'), '/', table, '_', field, '_', value, '.html'),
+                                                           paste0(normalizePath('./summaries/HTML'), '/', table, '_', field, '_', value, '.html'),
                                                            selfcontained = FALSE),
-                ~ htmlwidgets::saveWidget(., paste0(normalizePath('/app/summaries/HTML'), '/', table, '.html'),
+                ~ htmlwidgets::saveWidget(., paste0(normalizePath('./summaries/HTML'), '/', table, '.html'),
                                           selfcontained = FALSE)
     )
 }
